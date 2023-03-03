c0646489cf2cc703dd252c1b0b4e7daf7be376a9
Viacheslav Sukhenko <slavichmy@gmail.com>
2023-03-03 21:23:10 +0200

    create static version

--- hero.py
+++ hero.py
@@ -1,77 +1,66 @@
-
-
 class Hero():
     def __init__(self, pos, land):
         self.land = land
         self.mode = True
-        self.hero = loader.setModel(" ")
-        self.hero.setTexture(loader.loadTexture(" "))
-        self.hero.setScale(1)
+        self.hero = loader.loadModel('smiley')
+        self.hero.setScale(0.3)
+        self.hero.setColor(1, 0.5, 0)
         self.hero.setPos(pos)
         self.hero.reparentTo(render)
-    def camera_bind(self):
-        base.camera.setH(180) 
-        base.camera.reparentTo(self.hero) 
-        base.camera.setPos(0,0,1.5)
-        self.cameraOn = True 
-        base.disableMouse() 
+        self.cameraBind()
+        self.accept_events()
 
+    def cameraBind(self):
+        base.disableMouse()
+        base.camera.setH(180)
+        base.camera.reparentTo(self.hero)
+        base.camera.setPos(0, 0, 1.5)
+        self.cameraOn = True
     def cameraUp(self):
-        pos = self.hero.getPos() 
-        base.camera.reparentTo(render) 
-        base.enable.Mouse()
+        pos = self.hero.getPos()
+        base.mouseInterfaceNode.setPos(-pos[0], -pos[1], -pos[2]-3)
+        base.camera.reparentTo(render)
+        base.enableMouse()
         self.cameraOn = False
-        base.mouseInterfaceNode.setPos(pos[0], pos[1], pos[2]+3)
     def changeView(self): 
         if self.cameraOn:
             self.cameraUp()
         else:
-            self.camera_bind()
+            self.cameraBind()
     def turn_left(self):
         self.hero.setH((self.hero.getH() + 5) % 360)
     def turn_right(self):
         self.hero.setH((self.hero.getH() - 5) % 360)
 
     def look_at(self, angle):
-        x_from=round(self.hero.getX())
-        y_from=round(self.hero.getY())
-        z_from=round(self.hero.getZ())
+        x_from = round(self.hero.getX())
+        y_from = round(self.hero.getY())
+        z_from = round(self.hero.getZ())
 
         dx, dy = self.check_dir(angle)
-        x_to = x_from*dx
-        y_to = y_from*dy
+        x_to = x_from + dx
+        y_to = y_from + dy
         return x_to, y_to, z_from
-    def check_dir(self, angle):
-        if angle >= 0 and angle <= 28:
-            return(0, -1)
+    def check_dir(self,angle):
+        if angle >= 0 and angle <= 20:
+            return (0, -1)
         elif angle <= 65:
-            return(1, -1)
+            return (1, -1)
         elif angle <= 110:
-            return(1, 0)
+            return (1, 0)
         elif angle <= 155:
-            return(1, 1)
+            return (1, 1)
         elif angle <= 200:
-            return(0, 1)
+            return (0, 1)
         elif angle <= 245:
-            return(-1, 1)
+            return (-1, 1)
         elif angle <= 290:
-            return(-1, 0)
+            return (-1, 0)
         elif angle <= 335:
-            return(-1, -1)
+            return (-1, -1)
         else:
             return(0, -1)
-    def Grow(self):
-        self.land.block.setTexture(loader.loadTexture(self.texture1))
-        angle = (self.hero.getH())%360
-        pos = self.look_at(angle)
-        self.hero.setPos(pos)
-        self.land.add_Block(pos)
-    def Grow(self):
-        self.land.block.setTexture(loader.loadTexture(self.texture2))
-        angle = (self.hero.getH())%360
-        pos = self.look_at(angle)
-        self.hero.setPos(pos)
-        self.land.add_Block(pos)
+
     def forward(self):
         angle = (self.hero.getH())%360
         pos = self.look_at(angle)
@@ -97,35 +86,32 @@ class Hero():
     def build(self):
         angle = (self.hero.getH())%360
         pos = self.look_at(angle)
-        self.hero.setPos(pos)
-        self.land.add_Block(pos)
+        self.land.addBlock(pos)
     def destroy(self):
         angle = (self.hero.getH())%360
         pos = self.look_at(angle)
-        self.hero.setPos(pos)
-        self.land.del_Block(pos)
+        self.land.delBlock(pos)
     def accept_events(self):
-        base.accept("c", self.changeView)
-        base.accept("q", self.turn_left)
-        base.accept("q" + "-repeat",self.turn_right)
-        base.accept("e", self.turn_left)
-        base.accept("e" + "-repeat",self.forward)
-        base.accept("w", self.forward)
-        base.accept("w" + "-repeat",self.forward)
-        base.accept("s", self.back)
-        base.accept("s" + "-repeat",self.back)
-        base.accept("a", self.left)
-        base.accept("a" + "-repeat",self.left)
-        base.accept("d", self.right)
-        base.accept("d" + "-repeat",self.right)
-        base.accept("z", self.up)
-        base.accept("z" + "-repeat",self.up)
-        base.accept("x", self.down)
-        base.accept("x" + "-repeat",self.down)
-        base.accept("mouse1", self.build)
-        base.accept("mouse3", self.destroy)
-        base.accept("k", self.land.saveMap)
-        base.accept("l", self.land.loadMap)
-        base.accept("1", self.land.Grow)
-        base.accept("2", self.land.Stone)
+        base.accept('c', self.changeView)
+        base.accept('w', self.forward)
+        base.accept('w' + '-repeat', self.forward)
+        base.accept('d', self.right)
+        base.accept('d' + '-repeat', self.right)
+        base.accept('a', self.left)
+        base.accept('a' + '-repeat', self.left)
+        base.accept('s', self.back)
+        base.accept('s' + '-repeat', self.back)
+        base.accept('q', self.turn_left)
+        base.accept('q' + '-repeat', self.turn_left)
+        base.accept('e', self.turn_right)
+        base.accept('e' + '-repeat', self.turn_right)
+        base.accept('j', self.up)
+        base.accept('j' + '-repeat', self.up)
+        base.accept('k', self.down)
+        base.accept('k' + '-repeat', self.down)
+        base.accept('z', self.land.saveMap)
+        base.accept('x', self.land.loadMap)
+        base.accept('mouse1', self.build)
+        base.accept('mouse3', self.destroy)
+
 
